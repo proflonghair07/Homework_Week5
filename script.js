61723ac5c502e7a1bd32854f488b77b37cc8295e
omniclause <vaughnmurphy89@gmail.com>
2020-06-07 15:19:22 -0500

    Added readme, cleaned up code, and did some styling.

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// Globabl Variables
-console.log("farts");
-//  Display current date to header
-
-//  Change color of event input depending on time of day.  Different color for present future and past.
-
-//  Save input to local storage upon click of lock button
-// nine-am input field
+// Global variables
 var nineAmInput = document.getElementById("nine-am");
 var tenAmInput = document.getElementById("ten-am");
 var elevenAmInput = document.getElementById("eleven-am");
@@ -24,7 +17,6 @@ var twoPmButton = document.getElementById("two-pm-btn");
 var threePmButton = document.getElementById("three-pm-btn");
 var fourPmButton = document.getElementById("four-pm-btn");
 var fivePmButton = document.getElementById("five-pm-btn");
-var output = document.getElementById("output");
 
 // nine-am input field
 nineAmButton.addEventListener("click", function () {
@@ -36,17 +28,13 @@ nineAmButton.addEventListener("click", function () {
 function addInput() {
   if (localStorage.getItem("input-nine")) {
     let inputNine = localStorage.getItem("input-nine");
-    output.textContent = "saved";
     nineAmInput.placeholder = inputNine;
   } else {
     return;
   }
 }
 
-console.log(localStorage.getItem("input-nine"));
-document.body.onload = addInput;
-
-// // ten-am input field
+// ten-am input field
 
 tenAmButton.addEventListener("click", function () {
   localStorage.setItem("input-ten", tenAmInput.value);
@@ -56,16 +44,12 @@ tenAmButton.addEventListener("click", function () {
 
 function addInputTen() {
   if (localStorage.getItem("input-ten") || localStorage.getItem("input-nine")) {
-    let inputNine = localStorage.getItem("input-nine");
     let inputTen = localStorage.getItem("input-ten");
-    nineAmInput.placeholder = inputNine;
     tenAmInput.placeholder = inputTen;
   } else {
     return;
   }
 }
-
-document.body.onload = addInputTen;
 
 // eleven-am input field
 elevenAmButton.addEventListener("click", function () {
@@ -77,15 +61,11 @@ elevenAmButton.addEventListener("click", function () {
 function addInputEleven() {
   if (localStorage.getItem("input-eleven")) {
     let inputeleven = localStorage.getItem("input-eleven");
-    output.textContent = "saved";
     elevenAmInput.placeholder = inputeleven;
   } else {
     return;
   }
 }
-
-console.log(localStorage.getItem("input-eleven"));
-document.body.onload = addInputEleven;
 
 // twelve-pm input field
 twelvePmButton.addEventListener("click", function () {
@@ -97,15 +77,11 @@ twelvePmButton.addEventListener("click", function () {
 function addInputTwelve() {
   if (localStorage.getItem("input-twelve")) {
     let inputTwelve = localStorage.getItem("input-twelve");
-    output.textContent = "saved";
     twelvePmInput.placeholder = inputTwelve;
   } else {
     return;
   }
 }
-
-console.log(localStorage.getItem("input-twelve"));
-document.body.onload = addInputTwelve;
 
 // one-pm input field
 onePmButton.addEventListener("click", function () {
@@ -117,15 +93,11 @@ onePmButton.addEventListener("click", function () {
 function addInputOne() {
   if (localStorage.getItem("input-one")) {
     let inputone = localStorage.getItem("input-one");
-    output.textContent = "saved";
     onePmInput.placeholder = inputone;
   } else {
     return;
   }
 }
-
-console.log(localStorage.getItem("input-one"));
-document.body.onload = addInputOne;
 
 // two-pm input field
 twoPmButton.addEventListener("click", function () {
@@ -137,15 +109,11 @@ twoPmButton.addEventListener("click", function () {
 function addInputTwo() {
   if (localStorage.getItem("input-two")) {
     let inputtwo = localStorage.getItem("input-two");
-    output.textContent = "saved";
     twoPmInput.placeholder = inputtwo;
   } else {
     return;
   }
 }
-
-console.log(localStorage.getItem("input-two"));
-document.body.onload = addInputTwo;
 
 // three-pm input field
 threePmButton.addEventListener("click", function () {
@@ -157,15 +125,11 @@ threePmButton.addEventListener("click", function () {
 function addInputThree() {
   if (localStorage.getItem("input-three")) {
     let inputthree = localStorage.getItem("input-three");
-    output.textContent = "saved";
     threePmInput.placeholder = inputthree;
   } else {
     return;
   }
 }
-
-console.log(localStorage.getItem("input-three"));
-document.body.onload = addInputThree;
 
 // four-pm input field
 fourPmButton.addEventListener("click", function () {
@@ -177,15 +141,11 @@ fourPmButton.addEventListener("click", function () {
 function addInputFour() {
   if (localStorage.getItem("input-four")) {
     let inputfour = localStorage.getItem("input-four");
-    output.textContent = "saved";
     fourPmInput.placeholder = inputfour;
   } else {
     return;
   }
 }
-
-console.log(localStorage.getItem("input-four"));
-document.body.onload = addInputFour;
 
 // five-pm input field
 fivePmButton.addEventListener("click", function () {
@@ -219,7 +179,4 @@ function addInputFive() {
   }
 }
 
-console.log(localStorage.getItem("input-five"));
 document.body.onload = addInputFive;
-
-console.log(inputTwo.classValue);
